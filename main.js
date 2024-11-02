72c5e9693ec5301f0b806ce5f92500369c2b5159
negin <neginsouri7979@gmail.com>
2024-11-02 19:05:19 +0430

    get all of element with dom that use in future

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 import './style.css'
 
+document.addEventListener("DOMContentLoaded", () => {
+  const contactListElement = document.getElementById("contact-list");
+  const contactForm = document.getElementById("contactForm");
+  const contactModal = document.getElementById("contactModal");
+  const modalTitle = document.getElementById("modalTitle");
+  const editIdInput = document.getElementById("editId");
+  const contactNameInput = document.getElementById("contactName");
+  const contactPhoneInput = document.getElementById("contactPhone");
+  const contactEmailInput = document.getElementById("contactEmail");
+  const addContactBtn = document.getElementById("addContactBtn");
+  const NameInput = document.getElementById("name");
+  const PhoneInput = document.getElementById("phone");
+  const EmailInput = document.getElementById("email");
+  const saveBtn = document.getElementById("saveBtn");
+  const closeModalBtn = document.getElementById("closeModal");
+  const searchInput = document.getElementById("search");
